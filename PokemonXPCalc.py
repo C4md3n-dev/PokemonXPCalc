f99279b52e57aa3c94671d42cee90016df1759be
C4md3n-dev <drummer2of2@gmail.com>
2020-06-15 09:22:57 -0500

    updated PokemonXPCalc.py, implemted erratic xp group

--- PokemonXPCalc.py
+++ PokemonXPCalc.py
@@ -1,12 +1,16 @@
 #PokemonXPCalc is a python program created by Camden Harper to calculate the xp need to get to the defined level.
-#Erratic XP Group (n <= 50) Formula: N/A
-#Erratic XP Group (50 <= n <= 68) Formula: N/A
-#Erratic XP Group (68<= n <= 98) Formula: N/A
-#Erratic XP Group (98 <= n <= 100) Formula: N/A
+#Erratic XP Group (n <= 50) Formula: n^3(100-n) / 50
+#Erratic XP Group (50 <= n <= 68) Formula: n^3(150-n) / 100
+#Erratic XP Group (68<= n <= 98) Formula: n^3(1911-10n / 3) / 500
+#Erratic XP Group (98 <= n <= 100) Formula: n^3(160-n) / 100
 #Fast XP Group Formula: 4n^3 / 5.
 #Medium Fast XP Group Formula: n^3.
 #Medium Slow XP Group Formula: 6/5n^3 - 15n^2 + 100n - 140.
 #Slow XP Group Formula: 5n^3 / 4.
+
+#Author: Camden Harper
+#Version: 0.2
+#©2020
 
 wlv = 0
 clv = 0
@@ -20,27 +24,34 @@ def erratic():
     try:
         wlv = int(input("Enter Wanted Level: "))
         clv = int(input("Enter Current Level: "))
-        cxp1 = int(input("Enter Current XP: "))
+        cxp1 = int(input("Enter Current XP to Next Level: "))
         if clv <= 50:
             cxp2 = cxp1 + clv**3*(100-clv) / 50
-        if clv >= 50 and clv <= 68:
+        if clv > 50 and clv <= 68:
             cxp2 = cxp1 + clv**2*(150-clv) / 100
-        if clv >= 68 and clv <= 98:
-            clv**3*(1911-10*clv/3) / 500
-        if clv >= 98 and clv <= 100:
-            clv**3*(160-clv) / 100
+        if clv > 68 and clv <= 98:
+            cxp2 = clv**3*(1911-10*clv/3) / 500
+        if clv > 98 and clv <= 100:
+            cxp2 = clv**3*(160-clv) / 100
 
         if wlv <= 50:
-            wxp = wlv**3*(100-wlv) / 50
-        if wlv >= 50 and wlv <= 68:
-            wlv**2*(150-wlv) / 100
-        if wlv >= 68 and wlv <= 98:
-            wlv**3*(1911-10*wlv/3) / 500
-        if wlv >= 98 and wlv <= 100:
-            wlv**3*(160-wlv) / 100
+            wxp = (clv**3*(100-clv)) / 50
+        if wlv > 50 and wlv <= 68:
+            wxp = wlv**2*(150-wlv) / 100
+        if wlv > 68 and wlv <= 98:
+            wxp = wlv**3*(1911-10*wlv/3) / 500
+        if wlv > 98 and wlv <= 100:
+            wxp = wlv**3*(160-wlv) / 100
 
         print(cxp2)
         print(wxp - cxp2)
+        print("(1) Restart \n(2) Exit")
+        command = input()
+        if command == "2":
+            exit()
+        else:
+            main()
+            clear()
     except:
         pass
 
@@ -48,11 +59,18 @@ def fast():
     try:
         wlv = int(input("Enter Wanted Level: "))
         clv = int(input("Enter Current Level: "))
-        cxp1 = int(input("Enter Current XP: "))
+        cxp1 = int(input("Enter Current XP to Next Level: "))
         cxp2 = cxp1 + (4 * ((clv**3) / 5))
         wxp = 4 * ((wlv**3) / 5)
         print(cxp2)
         print(wxp - cxp2)
+        print("(1) Restart \n(2) Exit")
+        command = input()
+        if command == "2":
+            exit()
+        else:
+            main()
+            clear()
     except:
         pass
 
@@ -60,11 +78,18 @@ def medium_fast():
     try:
         wlv = int(input("Enter Wanted Level: "))
         clv = int(input("Enter Current Level: "))
-        cxp1 = int(input("Enter Current XP: "))
+        cxp1 = int(input("Enter Current XP to Next Level: "))
         cxp2 = cxp1 + clv**3
         wxp = wlv**3
         print(cxp2)
         print(wxp - cxp2)
+        print("(1) Restart \n(2) Exit")
+        command = input()
+        if command == "2":
+            exit()
+        else:
+            main()
+            clear()
     except:
         pass
 
@@ -72,11 +97,18 @@ def medium_slow():
     try:
         wlv = int(input("Enter Wanted Level: "))
         clv = int(input("Enter Current Level: "))
-        cxp1 = int(input("Enter Current XP: "))
+        cxp1 = int(input("Enter Current XP to Next Level: "))
         cxp2 = cxp1 + 6/5*clv**3 - 15*clv**2 + 100*clv-140
         wxp = 6/5*wlv**3 - 15*wlv**2 + 100*wlv-140
         print(cxp2)
         print(wxp - cxp2)
+        print("(1) Restart \n(2) Exit")
+        command = input()
+        if command == "2":
+            exit()
+        else:
+            main()
+            clear()
     except:
         pass
 
@@ -84,11 +116,18 @@ def slow():
     try:
         wlv = int(input("Enter Wanted Level: "))
         clv = int(input("Enter Current Level: "))
-        cxp1 = int(input("Enter Current XP: "))
+        cxp1 = int(input("Enter Current XP to Next Level: "))
         cxp2 = cxp1 + 5*clv**3 / 4
         wxp = 5*wlv**3 / 4
         print(cxp2)
         print(wxp - cxp2)
+        print("(1) Restart \n(2) Exit")
+        command = input()
+        if command == "2":
+            exit()
+        else:
+            main()
+            clear()
     except:
         pass
 
@@ -96,8 +135,7 @@ def main():
     print("(1) Erratic \n(2) Fast \n(3) Medium Fast \n(4) Medium Slow \n(5) Slow")
     xpgrp = int(input("Enter XP Group Number: "))
     if xpgrp == 1:
-        print("The Erratic XP Group has not been implemented yet....sorry :(")
-        #erratic()
+        erratic()
     if xpgrp == 2:
         fast()
     if xpgrp == 3:
